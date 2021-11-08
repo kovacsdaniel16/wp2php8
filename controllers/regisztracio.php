b6a2deb0bd85fdb58338c6ea413be0fd7d4b2fdd
kovacsdaniel16 <kovacsdanieel@gmail.com>
2021-11-08 14:41:27 +0100

    reg0.2

--- controllers/regisztracio.php
+++ controllers/regisztracio.php
@@ -3,10 +3,10 @@
 class Regisztracio_Controller //létrehoztuk a regisztráció kontroller osztályát
 {
 	public $baseName = 'regisztracio';  
-	public function main(array $vars) 
+	public function main(array $vars) {
 
-		$view = new View_Loader($this->baseName."_main");
-	}
+    $view = new View_Loader($this->baseName."_main");
+    }
 }
 
 ?>
