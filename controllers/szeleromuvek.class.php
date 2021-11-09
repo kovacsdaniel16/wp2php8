3733681160356f3cd3b49f4895a1b419aa5d2235
balto1990 <92270489+balto1990@users.noreply.github.com>
2021-11-09 11:03:40 +0100

    Update szeleromuvek.class.php

--- controllers/szeleromuvek.class.php
+++ controllers/szeleromuvek.class.php
@@ -1,2 +1,8 @@
 <?php
 
+interface Kiir {
+  public function kiir(); //Egy kiir interfacet létrehozunk, hogy utána beimplementáljuk az osztályba.
+  
+/*--------------------------------------------------------------------------------------------------*/
+
+ abstract class 
