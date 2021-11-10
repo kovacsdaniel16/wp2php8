5774fcd737d100bd66ce12083660f0f1c7800aac
kovacsdaniel16 <kovacsdanieel@gmail.com>
2021-11-10 10:23:35 +0100

    regisztracio, kilepes, belepes működik

--- models/regisztracio_model.php
+++ models/regisztracio_model.php
@@ -5,48 +5,40 @@ class Regisztracio_Model
 	public function get_data($vars)
 	{
 		$retData['eredmeny'] = "";
-    
 		try {
-			$connection = Database::getConnection(); //database osztály getConnection függvénye meghívja a kapcsolatot
-		      
-      // Ha nem kaptunk meg minden adatot ez MŰKÖDIK
       if($_POST['csaladi_nev'] == "" || $_POST['utonev'] == "" || $_POST['login'] == "" || $_POST['jelszo'] == "")
-      {
-        $retData['eredmeny']="ERROR";
-        $retData['uzenet']='Nem sikerült';
-      }
-  
-  
-      // Ha megkaptunk minden adatot hozzuk létre a felhasználót a táblában
-      else
-      {
-        //írok egy query-t itt lesz valami baj
-        /*a*/ $sqlInsert = "insert into felhasznalok(id, csaladi_nev, utonev, bejelentkezes, jelszo)
-                          values(0, :csaladi_nev, :utonev, :bejelentkezes, :jelszo)";
-
-        /*b*/ $stmt = $connection->query($sqlInsert); //$sth = $dbh->prepare($sql);
-    
-    
-        if($stmt->execute(Array(':csaladi_nev' => $_POST['csaladi_nev'], ':utonev' => $_POST['utonev'],
-                              ':bejelentkezes' => $_POST['login'], ':jelszo' => $_POST['jelszo'])))
         {
-          // Sikerült a létrehozás (insert)
-          $retData['eredmeny']="OK";
-          $retData['uzenet']='Sikerült';
-        }
+          $retData['eredmeny'] = "ERROR";
+					$retData['uzenet'] = "Nincs kitöltve az összes mező";        }
+        // Ha megkaptunk minden adatot hozzuk létre a felhasználót a táblában
         else
         {
-          // Nem sikerült a létrehozás
-          $retData['eredmeny']="ERROR";
-          $retData['uzenet']='Nem sikerült jajj';         
-        }
-      }
-		}
+			$connection = Database::getConnection();
+			$sql = "select id, csaladi_nev, utonev, jogosultsag from felhasznalok where bejelentkezes='".$vars['login']."' ";
+			$stmt = $connection->query($sql);
+			$felhasznalo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			switch(count($felhasznalo)) {
+				case 0:
+					$retData['eredmeny'] = "ERROR";
+					$retData['uzenet'] = "Regisztració sikeres " . $vars['login'] ;
+                    $sqlInsert = "insert into felhasznalok(id, csaladi_nev, utonev, bejelentkezes, jelszo, jogosultsag) values (0, :csaladinev, :utonev, :bejelentkezes, :jelszo, :jogosultsag  ) ";
+                    $stmt = $connection->prepare($sqlInsert); 
+                    $stmt->execute(array(':csaladinev' => $vars['csaladi_nev'], ':utonev' => $vars['utonev'],
+                    ':bejelentkezes' => $vars['login'],':jogosultsag' => "_1_" ,':jelszo' => sha1($vars['jelszo']))); 
+					break;
+				
+				default:
+					$retData['eredmeny'] = "ERROR";
+					$retData['uzenet'] = "Ezzel a felhasználónévvel már regisztráltak!";
+			}
+		}}
 		catch (PDOException $e) {
-					$retData['eredmény'] = "ERROR";
+					$retData['eredmeny'] = "ERROR" ;
 					$retData['uzenet'] = "Adatbázis hiba: ".$e->getMessage()."!";
 		}
+  
 		return $retData;
+  
 	}
 }
 
