bdce28e8c1e541cea64cfcc955fc08fac2d3b040
kovacsdaniel16 <kovacsdanieel@gmail.com>
2021-11-12 14:58:12 +0100

    web2.sql frissitve

--- views/page_main.php
+++ views/page_main.php
@@ -2,14 +2,14 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <title>MVC - PHP</title>
+        <title>Szélerőművek beadandó</title>
         <link rel="stylesheet" type="text/css" href="<?php echo SITE_ROOT?>css/main_style.css">
         <?php if($viewData['style']) echo '<link rel="stylesheet" type="text/css" href="'.$viewData['style'].'">'; ?>
     </head>
     <body>
         <header>
             <div id="user"><em><?= $_SESSION['userlastname']." ".$_SESSION['userfirstname']." (".$_SESSION['userlogin'].")" ?></em></div>
-            <h1 class="header">&nbsp;&nbsp;&nbsp;&nbsp;Magyarországi szélerőművek</h1>
+            <h1 class="header">&nbsp;Magyarországi szélerőművek</h1>
         </header>
         <nav>
             <?php echo Menu::getMenu($viewData['selectedItems']); ?>
@@ -20,6 +20,6 @@
         <section>
             <?php if($viewData['render']) include($viewData['render']); ?>
         </section>
-        <footer>&copy; NJE - GAMF - Informatika Tanszék <?= date("Y") ?></footer>
+        <footer>&nbsp;&nbsp;&nbsp;&nbsp;Készítette: &nbsp;&nbsp;&nbsp;&nbsp;Tóth Zsuzsanna és Kovács Dániel &nbsp;&nbsp;&nbsp;&nbsp;<?= date("Y") ?></footer>
     </body>
 </html>
--- web2.sql
+++ web2.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2018. Már 05. 23:47
--- Kiszolgáló verziója: 10.1.19-MariaDB
--- PHP verzió: 7.0.13
+-- Létrehozás ideje: 2021. Nov 12. 14:54
+-- Kiszolgáló verziója: 10.4.21-MariaDB
+-- PHP verzió: 8.0.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `web2`
 --
-CREATE DATABASE IF NOT EXISTS `web2` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `web2`;
 
 -- --------------------------------------------------------
 
@@ -28,31 +27,44 @@ USE `web2`;
 -- Tábla szerkezet ehhez a táblához `felhasznalok`
 --
 
-CREATE TABLE IF NOT EXISTS `felhasznalok` (
-  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+CREATE TABLE `felhasznalok` (
+  `id` int(10) UNSIGNED NOT NULL,
   `csaladi_nev` varchar(45) NOT NULL DEFAULT '',
   `utonev` varchar(45) NOT NULL DEFAULT '',
   `bejelentkezes` varchar(12) NOT NULL DEFAULT '',
   `jelszo` varchar(40) NOT NULL DEFAULT '',
-  `jogosultsag` varchar(3) NOT NULL DEFAULT '_1_',
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+  `jogosultsag` varchar(3) NOT NULL DEFAULT '_1_'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
 -- A tábla adatainak kiíratása `felhasznalok`
 --
 
 INSERT INTO `felhasznalok` (`id`, `csaladi_nev`, `utonev`, `bejelentkezes`, `jelszo`, `jogosultsag`) VALUES
-(1, 'Rendszer', 'Admin', 'Admin', sha1('admin'), '__1'),
-(2, 'Családi_2', 'Utónév_2', 'Login2', sha1('login2'), '_1_'),
-(3, 'Családi_3', 'Utónév_3', 'Login3', sha1('login3'), '_1_'),
-(4, 'Családi_4', 'Utónév_4', 'Login4', sha1('login4'), '_1_'),
-(5, 'Családi_5', 'Utónév_5', 'Login5', sha1('login5'), '_1_'),
-(6, 'Családi_6', 'Utónév_6', 'Login6', sha1('login6'), '_1_'),
-(7, 'Családi_7', 'Utónév_7', 'Login7', sha1('login7'), '_1_'),
-(8, 'Családi_8', 'Utónév_8', 'Login8', sha1('login8'), '_1_'),
-(9, 'Családi_9', 'Utónév_9', 'Login9', sha1('login9'), '_1_'),
-(10, 'Családi_10', 'Utónév_10', 'Login10', sha1('login10'), '_1_');
+(11, 'Töpörtyű', 'Tibor', 'ttibi', '7c222fb2927d828af22f592134e8932480637c0d', '_1_');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `hirek`
+--
+
+CREATE TABLE `hirek` (
+  `id` int(20) NOT NULL,
+  `hir` text NOT NULL,
+  `felhasznalo` text NOT NULL,
+  `time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- A tábla adatainak kiíratása `hirek`
+--
+
+INSERT INTO `hirek` (`id`, `hir`, `felhasznalo`, `time`) VALUES
+(324, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'kovacspista0', '2021-11-12 13:50:54'),
+(325, 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?', 'kovacspista0', '2021-11-12 13:51:15'),
+(326, 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?', 'kovacspista0', '2021-11-12 13:51:34'),
+(327, 'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?', 'kovacspista0', '2021-11-12 14:12:10');
 
 -- --------------------------------------------------------
 
@@ -60,13 +72,12 @@ INSERT INTO `felhasznalok` (`id`, `csaladi_nev`, `utonev`, `bejelentkezes`, `jel
 -- Tábla szerkezet ehhez a táblához `menu`
 --
 
-CREATE TABLE IF NOT EXISTS `menu` (
+CREATE TABLE `menu` (
   `url` varchar(30) NOT NULL,
   `nev` varchar(30) NOT NULL,
   `szulo` varchar(30) NOT NULL,
   `jogosultsag` varchar(3) NOT NULL,
-  `sorrend` tinyint(4) NOT NULL,
-  PRIMARY KEY (`url`)
+  `sorrend` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -75,14 +86,50 @@ CREATE TABLE IF NOT EXISTS `menu` (
 
 INSERT INTO `menu` (`url`, `nev`, `szulo`, `jogosultsag`, `sorrend`) VALUES
 ('admin', 'Admin', '', '001', 80),
-('alapinfok', 'Alapinfók', 'elerhetoseg', '111', 40),
 ('belepes', 'Belépés', '', '100', 60),
-('elerhetoseg', 'Elérhetőség', '', '111', 20),
-('kiegeszitesek', 'Kiegészítések', 'elerhetoseg', '011', 50),
+('hirek', 'Hírek', '', '011', 30),
 ('kilepes', 'Kilépés', '', '011', 70),
-('linkek', 'Linkek', '', '100', 30),
-('nyitolap', 'Nyitólap', '', '111', 10);
+('nyitolap', 'Nyitólap', '', '111', 10),
+('szeleromuvek', 'Szélerőművek', '', '111', 20);
 
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `hirek`
+--
+ALTER TABLE `hirek`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- A tábla indexei `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`url`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT a táblához `hirek`
+--
+ALTER TABLE `hirek`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=328;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
