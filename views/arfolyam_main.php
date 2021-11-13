990e33584ace66d8856f10ca441bb87a3897ba45
balto1990 <92270489+balto1990@users.noreply.github.com>
2021-11-13 11:07:18 +0100

    Update arfolyam_main.php
    A legkérdezésekhez szükséges kódolás hozzáadása

--- views/arfolyam_main.php
+++ views/arfolyam_main.php
@@ -1,5 +1,7 @@
 <h1>MNB Árfolyamok</h1>
 
-<h3>Napi árfolyam</h3>
+<h3>Napi árfolyam:</h3>
+<h4><br><?= $viewData['napi']?><br></h4>
 
-<h3>Havi árfolyam</h3>
+<h3>Havi árfolyam:</h3>
+<h4><br><?= $viewData['havi']?><br></h4>
